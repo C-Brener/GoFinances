f066a6ea1ef5f3d52513e1473f5147b81709b910
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2024-08-12 20:46:56 -0300

    chore: gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.ktlint.kotlin.android) apply false
+    alias(libs.plugins.kover.kotlin) apply false
 }
