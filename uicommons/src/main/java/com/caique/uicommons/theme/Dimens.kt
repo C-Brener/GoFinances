66cf9c6026770d394419ac9895c603bd1364ffba
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2024-08-25 17:55:44 -0300

    feat: create dimens file

---
+++ uicommons/src/main/java/com/caique/uicommons/theme/Dimens.kt
@@ -0,0 +1,17 @@
+package com.caique.uicommons.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+// Font Size
+val font_size_title = 14.sp
+
+// Spacing
+val spacing_medium = 16.dp
+val thin_size = 1.dp
+
+// Radius
+val button_radius = 5.dp
+
+// Button Size
+val button_size = 56.dp
