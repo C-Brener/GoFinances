3a592cb2fd684e0c0e6368ac3f209984cdb05928
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2024-10-02 22:52:03 -0300

    refactor: rename font size and add new spaces

--- uicommons/src/main/java/com/caique/uicommons/theme/Dimens.kt
+++ uicommons/src/main/java/com/caique/uicommons/theme/Dimens.kt
@@ -6,18 +6,21 @@ import androidx.compose.ui.unit.sp
 // Font Size
 val font_size_title = 14.sp
 val font_size_text = 12.sp
-val font_size_heading = 32.sp
+val font_size_heading_1 = 32.sp
+val font_size_heading_2 = 20.sp
 
 // Spacing
 val space_thirty_six = 36.dp
 val space_thirty_two = 32.dp
+val space_twenty_four = 24.dp
 val space_twenty = 20.dp
+val space_eight_teen = 16.dp
 val space_sixteen = 16.dp
 val space_six = 6.dp
 val thin_size = 1.dp
 
 // Radius
-val button_radius = 5.dp
+val radius_five = 5.dp
 
 // Button Size
 val button_size = 56.dp
