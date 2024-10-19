fc759ffa784932651e769ae56b92454dda6781ae
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2024-10-18 22:39:59 -0300

    feat: Create login screen

--- login/src/main/java/com/caique/login/LoginScreen.kt
+++ login/src/main/java/com/caique/login/LoginScreen.kt
@@ -2,8 +2,6 @@ package com.caique.login
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun LoginScreen(
